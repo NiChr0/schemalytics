6491f1fbc337c02d89f9878c7ef4ee052f187e0f
GfkW <116035376+NiChr0@users.noreply.github.com>
2026-02-01 17:09:13 +0100

    Downgrade version from 0.2.0 to 0.1.0

--- schemalytics/__init__.py
+++ schemalytics/__init__.py
@@ -3,7 +3,7 @@ from schemalytics.models import Schema, BusinessContext, ModelingPlan
 from schemalytics.extractors.postgres import extract_schema
 from schemalytics.generators.dbt import generate_dbt_project
 
-__version__ = "0.2.0"
+__version__ = "0.1.0"
 __all__ = [
     "Schema",
     "BusinessContext", 
