51bc96c0459778ec00c4de44bc109e6c67be5eab
NiChr0 <nchr_projects@posteo.com>
2026-04-12 18:22:24 +0200

    chore: bump version to 1.0.1

--- schemalytics/__init__.py
+++ schemalytics/__init__.py
@@ -3,7 +3,7 @@ from schemalytics.models import Schema, BusinessContext, ModelingPlan, PipelineC
 from schemalytics.extractors.postgres import extract_schema
 from schemalytics.generators.dbt import generate_dbt_project
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = [
     "Schema",
     "BusinessContext",
